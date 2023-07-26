1e70f2ce50ed856644334eeeae9207f9aa815e2a
John Doe <mohaiminur24@gmail.com>
2023-07-26 22:17:22 +0600

    task one solved

---
+++ taskOne.js
@@ -0,0 +1,15 @@
+// Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+// Example Input: "hello world" Example Output: "dlrow olleh"
+
+
+let result = []
+const rev = (str) => {
+  for (let i = str.length-1; i >= 0; i--) {
+    const element = str[i];
+    result.push(element);
+  }
+
+};
+rev("string");
+
+console.log(result.join(''));
